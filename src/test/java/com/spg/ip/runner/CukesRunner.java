23e3c150093afd9faf5f3ab1b3333a9ed893ba61
karthikreddy <karthikreddy2250@gmail.com>
2025-09-13 12:11:48 -0400

    "Adding cucumber reporting"

--- src/test/java/com/spg/ip/runner/CukesRunner.java
+++ src/test/java/com/spg/ip/runner/CukesRunner.java
@@ -13,5 +13,6 @@ import org.junit.platform.suite.api.Suite;
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "com.spg.ip.stepdefs")
 @ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME, value = "src/test/resources/features/cucumber.feature")
 @ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME,value = "@smoke")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports.html, json:target/cucumber.json")
 public class CukesRunner {
 }
